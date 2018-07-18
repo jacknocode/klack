b74830a6703b758c99124880145d35795b18d03e
kazuki-kachiri <kazuki.ikeda@kachiri.co.jp>
2018-07-18 18:05:59 +0900

    feat:#3-7/17owari,timelinemada

---
+++ components/sendbtn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="klackSendBtn">
+    <input type="submit" value="送信">
+  </div>
+</template>
+
+<style>
+.klackSendBtn {
+    height: 32px;
+    width: 60px;
+    background-color: #466acf;
+}
+input{
+  font-size: 18px;
+  text-align: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,13 @@
 <template>
   <section>
      <div class="title">{{ name }}</div>
-
-      <ul>
-        <li v-for="Message in Messages" :key="Message.id">{{ Message }}</li>
-      </ul>
-
-
-
+      <div class="message__body">
+        <p v-for="Message in Messages" :key="Message.id">{{ Message }}</p>
+      </div>
       <form @submit.prevent="addMessage">
        <input type="text" v-model="newMessage">
-       <input type="submit" value="送る">
-     </form>
+       <input type="submit" value="送信">
+      </form>
   </section>
 </template>
 
@@ -28,16 +24,41 @@ export default {
     addMessage: function(){
       this.Messages.push(this.newMessage);
       this.newMessage ="";
-    }
-  }
+    },
+    // sendMessage() {
+    //   let now = new Date()  // 現在時刻（世界標準時）を取得
+    //   now.setTime(now.getTime() + 1000 * 60 * 60 * 9) // 日本時間に変換
+    //   now = now.toJSON().split('T')[1].slice(0, 5)  // 時刻のみを取得
+    // }
+  },
 }
 </script>
 
 <style>
-.title {
-  color: red;
+section{
+  display: grid;
+  grid-template-rows: 80px 500px 60px 1fr;
+  grid-template-columns: 100px 300px;
 }
-li {
-  list-style-type: none;
-  }
+.title {
+  height: 80px;
+  width: 100px;
+  color: red;
+  border: solid 2px;
+}
+
+.message__body {
+  grid-row: 2;
+  grid-column: 2;
+  background-color: #d8d8d8;
+}
+form {
+  grid-row: 3;
+  grid-column: 2;
+  margin: auto;
+}
+form > input {
+  border-color: #eeaa2c;
+}
+
 </style>
